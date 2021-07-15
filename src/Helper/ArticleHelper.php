9cf9080174531285146a92e2471704ac7dcfb27c
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-07-15 20:42:34 +0200

    ArticleHelper. Remove wrong copyright

--- src/Helper/ArticleHelper.php
+++ src/Helper/ArticleHelper.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  mod_articles_category
- *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Access\Access;
