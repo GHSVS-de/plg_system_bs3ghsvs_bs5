6efa023773a74709e4aa42fd116cf722dbd7c925
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-07-18 00:55:57 +0200

    CS.

--- media/layouts/joomla/content/info_block/author.php
+++ media/layouts/joomla/content/info_block/author.php
@@ -1,10 +1,12 @@
 <?php
-defined('JPATH_BASE') or die;
+\defined('_JEXEC') or die;
+
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
-
 ?>
-<dt class="sr-only"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_AUTHOR'); ?></dt>
+<dt class="sr-only">
+	<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_AUTHOR'); ?>
+</dt>
 <dd class="createdby">
 	<?php $author = ($displayData['item']->created_by_alias ?: $displayData['item']->author); ?>
 	<?php if (!empty($displayData['item']->contact_link ) && $displayData['params']->get('link_author') == true) : ?>
--- media/layouts/joomla/content/info_block/block.php
+++ media/layouts/joomla/content/info_block/block.php
@@ -5,16 +5,20 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 ?>
 <dl class="article-info">
-	<?php if ($displayData['params']->get('show_author') && !empty($displayData['item']->author ))
+	<?php if ($displayData['params']->get('show_author')
+		&& !empty($displayData['item']->author ))
 	{ ?>
 		<?php echo HTMLHelper::_('bs3ghsvs.layout', 'joomla.content.info_block.author', $displayData); ?>
 	<?php
 	} ?>
 
 <?php
-$show_parent_category = $displayData['params']->get('show_parent_category') && !empty($displayData['item']->parent_slug);
+$show_parent_category = $displayData['params']->get('show_parent_category')
+	&& !empty($displayData['item']->parent_slug);
 $show_category = $displayData['params']->get('show_category');
-$showDate = $displayData['params']->get('show_publish_date') || $displayData['params']->get('show_create_date') || $displayData['params']->get('show_modified_date');
+$showDate = $displayData['params']->get('show_publish_date')
+	|| $displayData['params']->get('show_create_date')
+	|| $displayData['params']->get('show_modified_date');
 ?>
 
 <?php
@@ -29,8 +33,8 @@ $showDate = $displayData['params']->get('show_publish_date') || $displayData['pa
 			<dt class="sr-only"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_CATEGORIES'); ?></dt>
 		<?php
 		} ?>
-	
-	
+
+
 		<?php if ($show_parent_category) : ?>
 			<?php echo HTMLHelper::_('bs3ghsvs.layout', 'joomla.content.info_block.parent_category', $displayData); ?>
 		<?php endif; ?>
@@ -38,7 +42,7 @@ $showDate = $displayData['params']->get('show_publish_date') || $displayData['pa
 		<?php if ($show_category) : ?>
 			<?php echo HTMLHelper::_('bs3ghsvs.layout', 'joomla.content.info_block.category', $displayData); ?>
 		<?php endif; ?>
-	
+
 	<?php
 	}
 	else
@@ -62,13 +66,13 @@ $showDate = $displayData['params']->get('show_publish_date') || $displayData['pa
 		<?php echo Text::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', HTMLHelper::_('date', $displayData['item']->publish_up, Text::_('DATE_FORMAT_LC3'))); ?>
 	</dd>
 	<?php endif; ?>
-	
+
 	<?php if ($displayData['params']->get('show_create_date')) : ?>
 	<dd class="create">
 		<?php echo Text::sprintf('COM_CONTENT_CREATED_DATE_ON', HTMLHelper::_('date', $displayData['item']->created, Text::_('DATE_FORMAT_LC3'))); ?>
 	</dd>
 	<?php endif; ?>
-	
+
 	<?php if ($displayData['params']->get('show_modify_date')) : ?>
 	<dd class="modified">
 		<?php echo Text::sprintf('COM_CONTENT_LAST_UPDATED', HTMLHelper::_('date', $displayData['item']->modified, Text::_('DATE_FORMAT_LC3'))); ?>
