a375fbc37b2d2752b0dc3af7302c665ca5a398e0
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-09-20 16:42:26 +0200

    Simplify JLayout extension.php. Use $item->bs3ghsvsFields['extension']

--- media/layouts/ghsvs/extension.php
+++ media/layouts/ghsvs/extension.php
@@ -6,112 +6,127 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\Registry\Registry;
 
 /**
- * $displayData is item->id
+ * $item = article object.
 */
 
-JLoader::register(
-	'Bs3ghsvsArticle',
-	JPATH_PLUGINS . '/system/bs3ghsvs/Helper/ArticleHelper.php'
-);
+extract($displayData);
 
-JLoader::register(
-	'Bs3ghsvsItem',
-	JPATH_PLUGINS . '/system/bs3ghsvs/Helper/ItemHelper.php'
-);
-
-$displayData = Bs3ghsvsArticle::getExtensionData($displayData);
-
-if (false === $displayData)
+if (empty($item->bs3ghsvsFields['extension']['bs3ghsvs_extension_active']))
 {
-	return '';
+	return;
 }
 
-$displayData = new Registry($displayData);
+$extensionData = new Registry($item->bs3ghsvsFields['extension']);
 ?>
 <div class="block-download" aria-labelledby="EXTENSION_INFO">
 <h3 id="EXTENSION_INFO"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_INFO'); ?></h3>
 
-<p class="breakall" aria-label="<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_NAME'); ?>">
-	<?php echo Text::_($displayData->get('name')); ?>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_NAME'); ?></h4>
+<p class="breakall">
+	<?php echo Text::_($extensionData->get('name')); ?>
 </p>
 
-<p aria-label="<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_DESCRIPTION'); ?>">
-	<?php echo nl2br($displayData->get('description')); ?>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_DESCRIPTION'); ?></h4>
+<p>
+	<?php echo nl2br($extensionData->get('description')); ?>
 </p>
 
 <?php
-if (($out = trim($displayData->get('inspiredby'))))
-{ 
+if (($out = trim($extensionData->get('inspiredby'))))
+{
 	if (strpos($out, ' ') === false && Bs3ghsvsItem::hasScheme($out))
 	{
 		$out = '<a href="' . $out . '">' . $out . '</a>';
 	}
 ?>
-<p>
-	<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_INSPIREDBY'); ?>:
-	<?php echo $out; ?>
-</p>
+	<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_INSPIREDBY'); ?></h4>
+	<p>
+		<?php echo $out; ?>
+	</p>
 <?php
 } ?>
 
-<p aria-label="<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_URL_DESC'); ?>">
-	<a href="<?php echo $displayData->get('url'); ?>" class="btn spoilerButton"
-		title="<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_URL_DESC')?>">
-		<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_URL'); ?>
-		<span class="glyphicon glyphicon-download-alt" aria-hidden="true"> </span>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_URL'); ?></h4>
+<p>
+	<a href="<?php echo $extensionData->get('url'); ?>" class="btn btn-catcolor">
+		<span class="sr-only"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_URL_DESC')?></span>
+		{svg{bi/download}}
 	</a>
 </p>
 
 <?php
-if ($displayData->get('updateserver'))
-{ ?>
-<p aria-label="<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_UPDATESERVER'); ?>" >
-	<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_UPDATESERVER_'
-		. $displayData->get('updateserver')); ?>:
+// Is array or not exists if nothing selected in list.
+if (!empty($extensionData->get('tergetplatform')))
+{
+	$targetplatforms = $extensionData->get('tergetplatform');
+
+	foreach ($targetplatforms as $key => $version)
+	{
+		$targetplatforms[$key] = Text::_(
+			'PLG_SYSTEM_BS3GHSVS_EXTENSION_TERGETPLATFORM_' . $version
+		);
+	} ?>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_TERGETPLATFORM'); ?></h4>
+<p>
+	<?php echo implode(', ', $targetplatforms); ?>
 </p>
 <?php
 } ?>
 
 <?php
-if ($displayData->get('languages'))
-{
-	$flags = Bs3ghsvsArticle::buildFlagImages($displayData->get('languages'));	
-?>
+if ($extensionData->get('updateserver'))
+{ ?>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_UPDATESERVER'); ?></h4>
 <p>
-	<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_LANGUAGES'); ?>:
+	<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_UPDATESERVER_'
+		. $extensionData->get('updateserver')); ?>
+</p>
+<?php
+} ?>
+
+<?php
+if ($extensionData->get('languages'))
+{
+	$flags = Bs3ghsvsArticle::buildFlagImages($extensionData->get('languages'));
+?>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_LANGUAGES'); ?></h4>
+<p>
 	<?php echo implode(' ', $flags); ?>:
 </p>
 <?php
 } ?>
 
 <?php
-if (($out = trim($displayData->get('project'))))
+if (($out = trim($extensionData->get('project'))))
 { ?>
-<p aria-label="<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_PROJECT'); ?>">
-	<a href="<?php echo $out; ?>" class="btn spoilerButton">
-		<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_PROJECT'); ?>
-		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"> </span>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_PROJECT'); ?></h4>
+<p>
+	<a href="<?php echo $out; ?>" class="btn btn-catcolor">
+		<span class="sr-only"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_PROJECT_OPEN'); ?></span>
+		{svg{bi/link-45deg}}
 	</a>
 </p>
 <?php
 } ?>
 
 <?php
-if (($out = trim($displayData->get('comment'))))
+if (($out = trim($extensionData->get('comment'))))
 { ?>
-<p class="block" aria-label="<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_COMMENT'); ?>">
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_COMMENT'); ?></h4>
+<p>
 	<?php echo nl2br($out); ?>
 </p>
 <?php
 } ?>
 
 <?php
-if (($out = trim($displayData->get('history'))))
+if (($out = trim($extensionData->get('history'))))
 { ?>
+<h4 class="h6"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_HISTORY'); ?></h4>
 <p>
-	<a href="<?php echo $out; ?>" class="btn spoilerButton">
-		<?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_HISTORY'); ?>
-		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"> </span>
+	<a href="<?php echo $out; ?>" class="btn btn-catcolor">
+		<span class="sr-only"><?php echo Text::_('PLG_SYSTEM_BS3GHSVS_EXTENSION_HISTORY_OPEN'); ?></span>
+		{svg{bi/link-45deg}}
 	</a>
 </p>
 <?php
