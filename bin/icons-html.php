14761bc1fb6e38f220bf779b70917b6563b465e0
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-09-26 00:59:21 +0200

    bin/icons-html.php. Add <span> to SVGs for easier copy/paste actions.

--- bin/icons-html.php
+++ bin/icons-html.php
@@ -45,7 +45,7 @@ foreach ($collector as $svgFolder => $files)
 		$fileRel = mkShortPath($file);
 		$fileRel = str_replace('package/media/svgs/', '', $fileRel);
 		$html[] = '<div class=innercontainer>';
-		$html[] = '<div class="' . $class . '" style="font-size:4rem;color:blue;">'
+		$html[] = '<div class="' . $class . '" style="font-size:4rem;color:blue;"><span aria-hidden="true" class="svgSpan svg-lg">'
 			. file_get_contents($file)
 // . '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-medical-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 //  <path fill-rule="evenodd" d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zm-2.5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L5 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L7 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317V5.5zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
@@ -55,7 +55,7 @@ foreach ($collector as $svgFolder => $files)
 //  <path fill-rule="evenodd" d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zm-2.5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L5 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L7 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317V5.5zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
 //</svg>'
 
-			. '</div>
+			. '</span></div>
 			<p><b>' . $fileRel . '</b></p>
 			<p style="font-family:monospace">{svg{'
 			. $svgFolder . '/' . $saveFile
