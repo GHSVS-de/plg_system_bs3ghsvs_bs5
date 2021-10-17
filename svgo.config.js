eb18473940da279d37e51925ee371d561671478a
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-10-17 16:21:09 +0200

    Bugfix for black icons in BS v1.6.0

--- svgo.config.js
+++ svgo.config.js
@@ -6,24 +6,31 @@ module.exports = {
     pretty: true,
     indent: 2
   },
-  plugins: [{
-    name: 'preset-default',
-    params: {
-      overrides: {
-        cleanupListOfValues: true,
-        removeAttrs: {
-          attrs: [
-            'data-name',
-            'fill',
-            'clip-rule'
-          ]
-        },
-        removeUnknownsAndDefaults: {
-          keepRoleAttr: true
-        },
-        removeViewBox: false,
-        sortAttrs: true
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeUnknownsAndDefaults: {
+            keepRoleAttr: true
+          },
+          removeViewBox: false,
+          sortAttrs: true
+        }
+      }
+    },
+    // cleanupListOfValues and removeAttrs plugins are included in svgo
+    // but are not part of the preset-default, so we need to enable them separately
+    'cleanupListOfValues',
+    {
+      name: 'removeAttrs',
+      params: {
+        attrs: [
+          'clip-rule',
+          'data-name',
+          'fill'
+        ]
       }
     }
-  }]
+  ]
 }
