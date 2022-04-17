134037eeb062b5ba1e030952ab06f671aa5a4f5a
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-04-17 22:49:09 +0200

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,17 +1,4 @@
 <?php
-/*
-Siehe README.md
- */
-/*
-$topFilesFinder = PhpCsFixer\Finder::create()
-	->in(
-		[
-			__DIR__ . '/src-test/nur-test/mod_articles_category',
-		]
-	)
-	->files()
-	->depth(0);
-*/
 $mainFinder = PhpCsFixer\Finder::create()
 	->exclude('node_modules')
 	->exclude('build')
@@ -21,9 +8,7 @@ $mainFinder = PhpCsFixer\Finder::create()
 			__DIR__,
 		]
 
-	)
-	//->append($topFilesFinder)
-	;
+	);
 
 $config = new PhpCsFixer\Config();
 
